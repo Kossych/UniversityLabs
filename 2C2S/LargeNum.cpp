9f1ea2cca9bdea9deb9c9cfcf19b93cac6e1e253
Kossych <81522445+Kossych@users.noreply.github.com>
2021-05-27 18:59:08 +0700

    Update LargeNum.cpp

--- 2C2S/LargeNum.cpp
+++ 2C2S/LargeNum.cpp
@@ -7,19 +7,21 @@
 #define MAX_BASE ((unsigned long long)1<<BASE_SIZE)
 
 using namespace std;
-typedef unsigned char BASE;
+typedef unsigned int BASE;
 typedef conditional<BASE_SIZE<32,conditional<BASE_SIZE<16,unsigned short, unsigned int >::type, unsigned long long>::type TMP; 
-//typedef unsigned long long TMP;
+
 
 class LargeNum{
+    public:
     BASE *coef;
     int maxLEN;
     int len;
-    public:
+    
         LargeNum(int Len=1, bool k=0);
         LargeNum(const LargeNum &);
         ~LargeNum(){if(coef!=NULL) delete[]coef;}
         LargeNum& operator = (const LargeNum &);
+        LargeNum& operator = (const BASE);
 
         void HexInput(const char *);
         void HexOutput();
@@ -36,6 +38,8 @@ class LargeNum{
         LargeNum operator + (const BASE);
         LargeNum& operator += (const BASE);
 
+        LargeNum operator - (const BASE);
+        LargeNum& operator -= (const BASE);
         LargeNum operator - (const LargeNum &);
         LargeNum& operator -= (const LargeNum &);
 
@@ -57,6 +61,9 @@ class LargeNum{
     private:
         void ExpMaxLen(int);
         void NormLen();
+        void SubLN(const LargeNum&, int);
+        void AddLN(const LargeNum&, int);
+
 };
 
     LargeNum::LargeNum(int Len, bool k){
@@ -98,6 +105,13 @@ class LargeNum{
             for(int i=0;i<maxLEN;i++) coef[i]=LN.coef[i];
             return *this;
         }
+        return *this;
+    }
+
+    LargeNum& LargeNum::operator = (const BASE num){
+        len=1;
+        for(int i=1;i<maxLEN;i++) coef[i]=0;
+        coef[0]=num;
         return *this;
     }
 
@@ -266,6 +280,28 @@ class LargeNum{
         return *this;
     }
 
+    LargeNum LargeNum::operator - (const BASE num){
+        if(*this<num){cout<<"error\n"; return *this;}
+        TMP tmp;
+        bool k=0;
+        LargeNum A(*this);
+        tmp=A.coef[0]+MAX_BASE-num;
+        A.coef[0]=(BASE)tmp;
+        k=!((tmp)>>BASE_SIZE);
+        for(int i=A.len;k&&i<=A.len;i++){
+            tmp=A.coef[i]+MAX_BASE-k;
+            A.coef[i]=(BASE)tmp;
+            k=!((tmp)>>BASE_SIZE);
+        }
+        A.NormLen();
+        return A;
+    }
+    
+    LargeNum& LargeNum::operator -= (const BASE num){
+        *this=*this-num;
+        return *this;
+    }
+
     LargeNum LargeNum::operator - (const LargeNum &LN){
         if(*this<LN){cout<<"error\n"; return *this;}
         TMP tmp;
@@ -363,6 +399,52 @@ class LargeNum{
         return *this;
     }
 
+    LargeNum LargeNum::operator/(const LargeNum &LN){
+        LargeNum q(len-LN.len+1);   
+        if(*this==LN){ q=1; return q;}
+        if(*this<LN){q=0; return q;}
+        TMP b=MAX_BASE;
+        //D1
+        TMP d=b/(LN.coef[LN.len-1]+1);
+        LargeNum dU(*this);
+        LargeNum dV(LN);
+        dU=dU*d;
+        dV=dV*d;   
+        //D2
+        for(int j=len-LN.len;j>=0;j--){//D7(Цикл по j)
+            //D3
+            BASE _j=j+LN.len;
+            TMP _q=((dU.coef[_j]*b+dU.coef[_j-1])/dV.coef[LN.len-1]);
+            TMP _r=((dU.coef[_j]*b+dU.coef[_j-1])%dV.coef[LN.len-1]);
+            if((_q==b)||((_q*dV.coef[LN.len-2])>(b*_r+dU.coef[_j-2]))){
+                _q--; _r=_r+dV.coef[LN.len-1];
+                if((_r<b)&&((_q==b)||(_q*dV.coef[LN.len-2]>b*_r+dU.coef[_j-2]))){
+                    _q--;
+                }
+            }
+            //D5
+            q.coef[j]=_q;   
+            //D4
+            if(dU>=dV*_q){
+            dU.SubLN(dV*_q,j);
+            }   
+            else{
+                dU=dU.coef[LN.len+1]+1;
+                //D6
+                q.coef[j]--;
+                dU.AddLN(dV,j);
+            }
+        }
+        //D8
+        q.NormLen();
+        return q;
+    }
+
+    LargeNum& LargeNum::operator/=(const LargeNum &LN){
+        *this=*this/LN;
+        return *this;
+    }
+
     void LargeNum::DecInput(const char *S){
         TMP tmp=0;
         BASE k=0;
@@ -403,15 +485,41 @@ class LargeNum{
     
 
     BASE LargeNum::operator%(const BASE num){
-        BASE r=0;
-        TMP MAX=MAX_BASE%num;
-        for(int i=0;i<len;i++){
-            BASE rn=coef[i]%num;
-            for(int j=0;j<i;j++) rn=(rn*MAX)%num;
-            r=(r+rn)%num;
+        TMP tmp,k=0;
+        for(int i=len-1;i>=0;i--){
+            tmp=(k<<BASE_SIZE)+coef[i];
+            k=tmp%num;
         }
-        return r;
+        return k;
     }
+
+    void LargeNum::SubLN(const LargeNum &LN, int j){
+        TMP tmp;
+        bool k=0;
+        for(int i=0;i<LN.len;i++){
+            tmp=coef[i+j]+MAX_BASE-LN.coef[i]-k;
+            coef[i+j]=(BASE)tmp;
+            k=!((tmp)>>BASE_SIZE);
+        }
+        for(int i=LN.len;k&&i<=len;i++){
+            tmp=coef[i+j]+MAX_BASE-k;
+            coef[i+j]=(BASE)tmp;
+            k=!((tmp)>>BASE_SIZE);
+        }
+        NormLen();
+    }
+
+
+    void LargeNum::AddLN(const LargeNum &LN, int j){
+        bool k=0;
+        for(int i=0;i<LN.len-2  ;i++){
+            TMP tmp=coef[i+j]+LN.coef[i]+k;
+            k=bool(tmp>>BASE_SIZE);
+            coef[i+j]=tmp;
+        }
+        NormLen();
+    }
+
 
     void LargeNum::ExpMaxLen(int b){
         maxLEN+=b;
@@ -441,51 +549,14 @@ int main()
     LargeNum B(4,0);
     LargeNum C;
  
-    //A.HexInput("ffffff");
-    A.DecInput("2573904118");
-    A.PrintCoef();
-    //B.DecInput("99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999");
-   // C=A*B;
-    //A.HexOutput();
-    //B.HexInput("301A");
-    //A.HexInput("A");
-    //B*=243;
-    //B.DecInput("1234123412");
-   // C.DecOutput();
-    //B.DecOutput();
-    //A.HexInput("ffff642");
-    //A.HexOutput();
-    //cout<<'\n';
-    //B.HexOutput();
-    //cout<<'\n';
-   // LargeNum C;
-    //C=A+B;
-    //C.HexOutput();
-    //cout<<'\n';
-    //C.HexOutput();
-    //cout<<'\n';
-   // C-=A;
-    //C.HexOutput();
-    //cout<<'\n';
+    //A.HexInput("7BFCE01DC4");
+    //B.HexInput("FB1F2833");
+    A.DecInput("94324782358238523857239857239085723985");
+    BASE r=A%325;
+    //A.PrintCoef();
+    B.DecInput("6436346346345786796087934534523");
+    C=A/B;
+    //B.PrintCoef();
+    C.DecOutput();
     
-    
-    //C=A+B;
-    //C=A-B;
-    //A+=B;
-    //A-=B;
-    //C=A*B;
-    //C=A/15;
-    //A*=B;
-
-
-   // C.HexOutput();
-    //cout<<'\n';
-    //A.HexOutput();
-    //cout<<'\n';
-    //B.HexOutput();
-    //cout<<'\n';
-
-  //  cout<<(A==B);
-
-    //system("pause");
 }
